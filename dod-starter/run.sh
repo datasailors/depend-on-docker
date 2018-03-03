3ee4e5d1d28dbecfda4882c9e31ee9cd2a0061f1
Alex Iankoulski <iankouls@ge.com>
2018-03-03 04:48:55 -0800

    Add proxy environment to run

--- dod-starter/run.sh
+++ dod-starter/run.sh
@@ -8,5 +8,5 @@ else
 	MODE=-it
 fi 
 
-docker container run ${CONTAINER_MAP} ${MODE} ${NETWORK} ${PORT_MAP} ${VOL_MAP} ${REGISTRY}${IMAGE}${TAG} $@
+docker container run -e http_proxy=$http_proxy -e https_proxy=$https_proxy -e no_proxy=$no_proxy ${CONTAINER_MAP} ${MODE} ${NETWORK} ${PORT_MAP} ${VOL_MAP} ${REGISTRY}${IMAGE}${TAG} $@
 
