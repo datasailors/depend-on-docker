d9816f6307fe935439bc304e1d9425cf8de96d9c
Alex Iankoulski <iankouls@ge.com>
2018-05-25 15:58:03 -0700

    Handle proxy settings

--- create.sh
+++ create.sh
@@ -11,15 +11,6 @@ export dod_url=https://github.com/iankoulski/depend-on-docker/trunk/linux/
 echo ""
 echo "Copying ${dod_url} to ${dest} ..."
 echo ""
-# Uncomment these lines to enable authentication
-#echo -n "[Username]:"
-#read username
-#echo -n "[Password]:"
-#read -s password
-#echo ""
-#echo ""
 
-docker container run --rm -d -v ${dest}:/wd iankoulski/svn sh -c "svn checkout --trust-server-cert --non-interactive ${dod_url} /wd && rm -rf /wd/.svn"
-#docker container run --rm -it -v ${dest}:/wd iankoulski/svn sh -c "svn checkout --trust-server-cert --non-interactive ${dod_url} /wd && rm -rf /wd/.svn"
-#docker container run --rm -it -v ${dest}:/wd iankoulski/svn sh -c "svn checkout --trust-server-cert --non-interactive --username ${username} --password ${password} ${dod_url} /wd && rm -rf /wd/.svn"
+docker container run --rm --name dod-create -d -e http_proxyHost=$http_proxyHost -e http_proxyPort=$http_proxyPort -v ${dest}:/wd iankoulski/svn sh -c "if [ ! -z "$http_proxyHost" ]; then (mkdir -p ~/.subversion; echo [global] | tee ~/.subversion/servers; echo http-proxy-host=${http_proxyHost} | tee -a ~/.subversion/servers; echo http-proxy-port=${http_proxyPort} | tee -a ~/.subversion/servers); fi; svn checkout --trust-server-cert --non-interactive ${dod_url} /wd && rm -rf /wd/.svn" && docker container logs -f dod-create
 
