b98f6ff0bd19cebe8fb5916286d098c0a90e3f13
Alex Iankoulski <iankouls@ge.com>
2018-05-25 09:34:59 -0700

    Disable authentication to prepare repo for becoming public at DockerCon 2018

--- create.sh
+++ create.sh
@@ -6,18 +6,19 @@ else
 	dest=$1
 fi
 
-export dod_url=https://github.build.ge.com/200018807/depend-on-docker/trunk/linux/
+export dod_url=https://github.com/iankoulski/depend-on-docker/trunk/linux/
 
 echo ""
 echo "Copying ${dod_url} to ${dest} ..."
 echo ""
-echo -n "[Username]:"
-read username
-echo -n "[Password]:"
-read -s password
-echo ""
-echo ""
+# Uncomment these lines to enable authentication
+#echo -n "[Username]:"
+#read username
+#echo -n "[Password]:"
+#read -s password
+#echo ""
+#echo ""
 
-#docker container run --rm -it -v ${dest}:/wd iankoulski/svn sh -c "svn checkout ${dod_url} /wd"
-docker container run --rm -it -v ${dest}:/wd iankoulski/svn sh -c "svn checkout --trust-server-cert --non-interactive --username ${username} --password ${password} ${dod_url} /wd && rm -rf /wd/.svn"
+docker container run --rm -it -v ${dest}:/wd iankoulski/svn sh -c "svn checkout --trust-server-cert --non-interactive ${dod_url} /wd && rm -rf /wd/.svn"
+#docker container run --rm -it -v ${dest}:/wd iankoulski/svn sh -c "svn checkout --trust-server-cert --non-interactive --username ${username} --password ${password} ${dod_url} /wd && rm -rf /wd/.svn"
 
