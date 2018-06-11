27dc0445ec0d05d71efce84433b289c1abd4da37
iankouls <40183636+iankouls@users.noreply.github.com>
2018-06-11 15:39:15 -0700

    bhgedigital org
    DockerCon 2018 release

--- create.bat
+++ create.bat
@@ -10,5 +10,5 @@ if not exist %dest% (
 	md %dest%
 )
 
-docker container run --rm -d -v "%dest%":"c:\wd" iankoulski/win-svn cmd /C "cd c:\wd && svn checkout https://github.com/iankoulski/depend-on-docker/trunk/windows/"
+docker container run --rm -d -v "%dest%":"c:\wd" bhgedigital/win-svn cmd /C "cd c:\wd && svn checkout https://github.com/bhgedigital/depend-on-docker/trunk/windows/"
 
